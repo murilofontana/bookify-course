f7b681a8ff2d3a9349610e588fddcb7cf462a91a
Murilo Fontana <fontana.murilo@gmail.com>
2025-03-15 17:16:06 -0300

    feat(backend): infrastructure project

---
+++ Bookify.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,60 @@
+﻿using Bookify.Application.Exceptions;
+using Bookify.Domain.Abstractions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookify.Infrastructure;
+
+public sealed class ApplicationDbContext : DbContext, IUnitOfWork
+{
+    private IPublisher _publisher;
+    public ApplicationDbContext(DbContextOptions options, IPublisher publisher) : base(options)
+    {
+        _publisher = publisher;
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Scan the assembly that contains the ApplicationDbContext and Apply all configuration from that assembly
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        
+        base.OnModelCreating(modelBuilder);
+    }
+
+    public override async Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            var result = await base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+
+            await PublishDomainEventsAsync();
+
+            return result;
+        }
+        catch (DbUpdateConcurrencyException ex)
+        {
+            throw new ConcurrencyException("Concurrency exception occurred.", ex);
+        }
+    }
+
+    private async Task PublishDomainEventsAsync()
+    {
+        var domainEvents = ChangeTracker
+             .Entries<Entity>()
+             .Select(entry => entry.Entity)
+             .SelectMany(entity =>
+             {
+                 var domainEvents = entity.GetDomainEvents();
+
+                 entity.ClearDomainEvents();
+
+                 return domainEvents;
+             })
+             .ToList();
+
+        foreach (var domainEvent in domainEvents)
+        {
+            await _publisher.Publish(domainEvent);
+        }
+    }
+}
---
+++ Bookify.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Data;
+using Bookify.Domain.Users;
+
+namespace Bookify.Infrastructure.Repositories;
+
+internal sealed class UserRepository : Repository<User>, IUserRepository
+{
+    public UserRepository(ApplicationDbContext dbContext)
+        : base(dbContext)
+    {
+    }
+}
