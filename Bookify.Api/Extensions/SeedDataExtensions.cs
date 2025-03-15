9abdc16424997a6ddecc824d21d4cb3999331cbd
Murilo Fontana <fontana.murilo@gmail.com>
2025-03-15 18:22:01 -0300

    fix(backend): fixing docker compose and seed data extensions

--- Bookify.Api/Extensions/SeedDataExtensions.cs
+++ Bookify.Api/Extensions/SeedDataExtensions.cs
@@ -41,7 +41,7 @@ internal static class SeedDataExtensions
 
         const string sql = """
             INSERT INTO public.apartments
-            (id, "name", description, address_country, address_state, address_zip_code, address_city, address_street, price_amount, price_currency, cleaning_fee_amount, cleaning_fee_currency, amenities, last_booked_on_utc)
+            (id, "name", description, address_country, address_state, address_zip_code, address_city, address_street, price_amount, price_currency, cleaning_fee_amount_amount, cleaning_fee_amount_currency, amenities, last_booked_on_utc)
             VALUES(@Id, @Name, @Description, @Country, @State, @ZipCode, @City, @Street, @PriceAmount, @PriceCurrency, @CleaningFeeAmount, @CleaningFeeCurrency, @Amenities, @LastBookedOn);
             """;
 
