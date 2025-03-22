6cc1897cf8124f963147b1714f0d403c6f7ba5d3
Murilo Fontana <fontana.murilo@gmail.com>
2025-03-21 23:00:46 -0300

    feat(backend): adding integration tests

--- Bookify.Api/Program.cs
+++ Bookify.Api/Program.cs
@@ -45,7 +45,7 @@ if (app.Environment.IsDevelopment())
 
     app.ApplyMigrations();
 
-    //app.SeedData();
+    app.SeedData();
 }
 
 app.UseHttpsRedirection();
@@ -69,3 +69,5 @@ app.MapHealthChecks("health", new HealthCheckOptions
 
 
 app.Run();
+
+public partial class Program;
